a03df3a617563f31a4eecb1af174d237a91e2661
harrychen <harrychen1995@yahoo.com>
2019-12-26 19:06:01 -0500

    ok

--- linear_regression.js
+++ linear_regression.js
@@ -1,5 +1,4 @@
 const tf = require("@tensorflow/tfjs")
-
 var X = []
 
 for (var i=0 ; i< 10; i++){
@@ -9,20 +8,17 @@ var Y = []
 
 X.forEach((d)=>{Y.push(10*d+2)})
 var Y_noise = []
-Y.forEach((d)=>{Y_noise.push(d+Math.random())})
-console.log(Y_noise)
+Y.forEach((d)=> {Y_noise.push(d+Math.random())})
 
 X = tf.tensor(X)
 Y = tf.tensor(Y)
 const A = tf.scalar(Math.random()).variable();
 const B = tf.scalar(Math.random()).variable();
-A.print()
-B.print()
 
-const pred = (x) => tf.tidy(()=>A.mul(x).add(B))
+const pred = (x) => A.mul(x).add(B)
 
 
-const loss = (predict, label)=> tf.tidy(()=>predict.sub(label).square().mean())
+const loss = (predict, label)=> predict.sub(label).square().mean()
 
 
 const optimizer = tf.train.sgd(0.01)
@@ -32,13 +28,14 @@ for (var i = 0; i < 1000; i++){
     optimizer.minimize(()=>loss(pred(X), Y_noise))
 }
 
-console.log(
-    `a: ${A.dataSync()}, B: ${B.dataSync()}`);
+var s  = `A: ${A.dataSync()}, B: ${B.dataSync()}\n`;
 const preds = pred(X).dataSync();
 preds.forEach((pred, i) => {
-  console.log(`X: ${i}, pred: ${pred}`);
+  s += `X: ${i}, pred: ${pred}` + "\n";
 });
 
+
+console.log(s)
 
 
 
